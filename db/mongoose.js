8b3ad3440819c0dfa17f3d2008ed54f635f2741d
AbdullahAlam22 <ala97953@gmail.com>
2020-04-18 01:31:15 +0600

    updating data base link

--- db/mongoose.js
+++ db/mongoose.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
      
-var mongouRI = 'mongodb+srv://yusef:1234@nodeapi-1-qpvve.mongodb.net/Todo?retryWrites=true&w=majority'
+var mongouRI = 'mongodb+srv://yusef:<password>@nodeapi-1-qpvve.mongodb.net/test?retryWrites=true&w=majority'
 var localDB = 'mongodb://localhost:27017/TodoApp';
 mongoose.connect(
-     'mongodb+srv://yusef:1234@nodeapi-1-qpvve.mongodb.net/Todo?retryWrites=true&w=majority',
+     'mongodb+srv://yusef:<password>@nodeapi-1-qpvve.mongodb.net/test?retryWrites=true&w=majority',
     { useNewUrlParser: true, useUnifiedTopology: true  }  
     );
 
